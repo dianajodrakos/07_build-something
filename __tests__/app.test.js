0a90355da511da182880e19143c28ec16f36798d
DJ Drakos <dianajodrakos@gmail.com>
2021-07-28 16:37:07 -0700

    created blank test and POST route

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,14 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Cry from '../lib/models/Cry.js';
 
-describe('demo routes', () => {
+describe('routes', () => {
   beforeEach(() => {
     return setup(pool);
+  });
+
+  it('POSTS a new cry', async () => {
+
   });
 });
