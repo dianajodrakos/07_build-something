377f7125adfc727bd0e6f9c1b3c0568a1d5d364e
DJ Drakos <dianajodrakos@gmail.com>
2021-07-28 17:46:04 -0700

    debugging auto-generated date functionality

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,6 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
-import Cry from '../lib/models/Cry.js';
 
 describe('routes', () => {
   beforeEach(() => {
@@ -10,16 +9,20 @@ describe('routes', () => {
   });
 
   it('POSTS a new cry', async () => {
+    const currentDate = new Date().toISOString().slice(0, 10);
     const cry = {
       name: 'DJ',
       cry: true,
+      date: currentDate
     };
 
-    const res = await (await request(app).post('api/v1/alchemy-cry-lab')).send(cry);
-
+    const res = await request(app)
+      .post('/api/v1/alchemy-cry-lab')
+      .send(cry);
+    console.log(res);
     expect(res.body).toEqual({
       id: '1',
-      date: '07-28-2021'
+      date: '07-28-2021',
       ...cry,
     });
   });
